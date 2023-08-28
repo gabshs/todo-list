31e2cb5dd13810f54e47f8d9c00dff1ef2a6cac7
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-28 17:22:09 -0300

    feat: creates fake tasks and pass to task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { ITask } from "./@types/task";
 import { Header } from "./components/Header";
 import { InputTask } from "./components/InputTask";
 import { TaskList } from "./components/TaskList";
 import "./global.css";
+
+const tasks: ITask[] = [
+  {
+    id: 1,
+    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isSelected: false,
+  },
+  {
+    id: 2,
+    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isSelected: true,
+  },
+  {
+    id: 3,
+    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isSelected: false,
+  },
+];
 
 function App() {
   return (
     <>
       <Header />
       <InputTask />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </>
   );
 }
