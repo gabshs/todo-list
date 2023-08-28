9b957695c1d9c570df1fb10934c9fbdfd54d518f
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-28 17:49:34 -0300

    feat: returns total of tasks and total of completed tasks dynamically

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -7,16 +7,20 @@ interface TaskListProps {
 }
 
 export function TaskList({ tasks }: TaskListProps) {
+  const totalOfTasks = tasks.length;
+  const totalOfCompletedTasks = tasks.filter((task) => task.isSelected).length;
   return (
     <main className={styles.container}>
       <header className={styles.header}>
         <div>
           <h3>Tarefas criadas</h3>
-          <span className={styles.counter}>5</span>
+          <span className={styles.counter}>{totalOfTasks}</span>
         </div>
         <div>
           <h3>Concluídas</h3>
-          <span className={styles.counter}>2 de 5</span>
+          <span className={styles.counter}>
+            {totalOfCompletedTasks} de {totalOfTasks}
+          </span>
         </div>
       </header>
       <div className={styles.taskList}>
