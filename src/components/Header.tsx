4e29f7cb926054fe63b5c61803411ba1231cbe61
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-27 21:47:16 -0300

    feat: creates header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo.svg'
+
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <img src={logo} alt="todo list" />
+    </header>
+  );
+}
