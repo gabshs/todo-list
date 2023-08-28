5b72bd15f7077607d7bbded6d4a37f284da38d8c
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-27 22:31:47 -0300

    feat: creates task list component

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,19 @@
+import styles from "./TaskList.module.css";
+
+export function TaskList() {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <div>
+          <h3>Tarefas criadas</h3>
+          <span className={styles.counter}>5</span>
+        </div>
+        <div>
+          <h3>Concluídas</h3>
+          <span className={styles.counter}>2 de 5</span>
+        </div>
+      </header>
+      <div></div>
+    </main>
+  );
+}
