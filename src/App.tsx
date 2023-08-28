46e96e873ee8278437600641ad48b2406dbce81e
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-27 22:32:05 -0300

    feats: adds task list to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Header } from './components/Header'
-import './global.css'
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
+import "./global.css";
 
 function App() {
-
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <TaskList />
+    </>
+  );
 }
 
-export default App
+export default App;
