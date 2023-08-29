1a5d19f4031ba6ad74440210d0addb0d1942c52d
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-28 22:18:22 -0300

    feat: adds function to create a new task

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,31 @@
 import { PlusCircle } from "phosphor-react";
 import styles from "./InputTask.module.css";
+import { ChangeEvent, useState } from "react";
 
-export function InputTask() {
+interface InputTaskProps {
+  onCreateNewTask: (task: string) => void;
+}
+
+export function InputTask({ onCreateNewTask }: InputTaskProps) {
+  const [newTaskInput, setNewTaskInput] = useState("");
+
+  function handleCreateNewText() {
+    onCreateNewTask(newTaskInput);
+  }
+
+  function handleNewTaskInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskInput(event.target.value);
+  }
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button>
+      <input
+        onChange={handleNewTaskInputChange}
+        value={newTaskInput}
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+      <button disabled={newTaskInput.length < 3} onClick={handleCreateNewText}>
         Criar <PlusCircle weight="bold" size={16} />
       </button>
     </div>
