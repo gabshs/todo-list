db99e47d117b85cd3eef621ced5790df645d298a
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-08-28 22:19:03 -0300

    feat: changes tasks mocked to useState and creates method to add a new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
+import { useState } from "react";
 import { ITask } from "./@types/task";
 import { Header } from "./components/Header";
 import { InputTask } from "./components/InputTask";
 import { TaskList } from "./components/TaskList";
 import "./global.css";
 
-const tasks: ITask[] = [
-  {
-    id: 1,
-    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isSelected: false,
-  },
-  {
-    id: 2,
-    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isSelected: true,
-  },
-  {
-    id: 3,
-    label: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
-    isSelected: false,
-  },
-];
-
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function createNewTask(newTask: string) {
+    const task: ITask = {
+      id: tasks.length + 1,
+      label: newTask,
+      isSelected: false,
+    };
+
+    setTasks([...tasks, task]);
+  }
   return (
     <>
       <Header />
-      <InputTask />
+      <InputTask onCreateNewTask={createNewTask} />
       <TaskList tasks={tasks} />
     </>
   );
